1814619e723258b2a7762abb18201fcd299e1604
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-15 22:18:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
-  array.each do |word|
+  array.collect do |word|
     new_array << word.reverse
   end
-  return new_array.join(" ")
+  return array.join(" ")
 end
